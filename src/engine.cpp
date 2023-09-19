75f3831ad4b6a75f0af60a434641711008408983
Rishabh Verma <10rishabhv@gmail.com>
2023-09-20 03:44:55 +0530

    fix alpha beta pruning

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <chrono>
 #include <random>
 #include <iostream>
 #include <climits>
@@ -19,7 +20,7 @@ const int BISHOP_WEIGHT = 300;
 const int KING_WEIGHT = 100000000;
 const int CHECK_WEIGHT = 500;
 const int ATTACKING_FACTOR = 6;
-const int DEFENDING_FACTOR = 2;
+const int DEFENDING_FACTOR = 4;
 
 unordered_map<string, int> mp;
 
@@ -39,10 +40,10 @@ int eval(Board& b) {
     U8 *player_pieces, *opponent_pieces;
     player_pieces = (curr_player == WHITE ? white_pieces : black_pieces);
     opponent_pieces = (curr_player == WHITE ? black_pieces : white_pieces);
-    // int promo_x = (curr_player == WHITE ? 4 : 2);
-    // int promo_y = (curr_player == WHITE ? 5 : 0);
-    // int kx = getx(white_pieces[2]);
-    // int ky = gety(white_pieces[2]);
+    int promo_x = (curr_player == WHITE ? 4 : 2);
+    int promo_y = (curr_player == WHITE ? 5 : 0);
+    int kx = getx(white_pieces[2]);
+    int ky = gety(white_pieces[2]);
 
     if (b.in_check()) {
         score -= CHECK_WEIGHT;
@@ -50,31 +51,32 @@ int eval(Board& b) {
 
     for (int i = 0; i < 6; i++) {
         if (player_pieces[i] != DEAD) {
-            // int tempx,tempy;
-            // tempx = getx(white_pieces[i]);
-            // tempy = gety(white_pieces[i]);
             score += weights[i];
-            // score += weights[i] + (weights[i]/(20+abs(ky-tempy)+(abs(kx-tempx))));
-            // if (i >= 4){
-            //     int px, py;
-            //     px = getx(player_pieces[i]);
-            //     py = gety(player_pieces[i]);
-            //     if (min(abs(py - promo_y), abs(py - promo_y - 1)) <= 1){
-            //         score += 400 / (1 + abs(px - promo_x));
-            //     } else {
-            //         score += 80 / (1 + abs(px - promo_x) + min(abs(py - promo_y), abs(py - promo_y - 1)));
-            //     }
-            // }
+
+            int tempx,tempy;
+            tempx = getx(white_pieces[i]);
+            tempy = gety(white_pieces[i]);
+            score += weights[i] / (20 + abs(ky - tempy) + abs(kx - tempx));
+            if (i >= 4){
+                int px, py;
+                px = getx(player_pieces[i]);
+                py = gety(player_pieces[i]);
+                if (min(abs(py - promo_y), abs(py - promo_y - 1)) <= 1){
+                    score += 400 / (1 + abs(px - promo_x));
+                } else {
+                    score += 80 / (1 + abs(px - promo_x) + min(abs(py - promo_y), abs(py - promo_y - 1)));
+                }
+            }
         }
         if (opponent_pieces[i] != DEAD) {
             score -= weights[i];
         }
     }
 
-    // unordered_set<U16> player_moves = b.get_legal_moves();
+    unordered_set<U16> player_moves = b.get_legal_moves();
 
 
-    // for (auto move : player_moves) {
+    for (auto move : player_moves) {
         // Board* new_board = b.copy();
         // new_board->do_move(move);
         // unordered_set<U16> opponent_moves = new_board->get_legal_moves();
@@ -82,29 +84,25 @@ int eval(Board& b) {
         // if (opponent_moves.empty()) {
         //     score += KING_WEIGHT;
         // }
-        // U8 final_pos = getp1(move);
-        // for (int i = 0; i < 6; i++) {
-        //     if (final_pos == opponent_pieces[i]) {
-        //         score += weights[i] / ATTACKING_FACTOR;
-        //     }
-        // }
-    // }
+        U8 final_pos = getp1(move);
+        for (int i = 0; i < 6; i++) {
+            if (final_pos == opponent_pieces[i]) {
+                score += weights[i] / ATTACKING_FACTOR;
+            }
+        }
+    }
 
-    // flip_player(b);
-    // unordered_set<U16> opponent_moves = b.get_legal_moves();
-    // flip_player(b);
+    flip_player(b);
+    unordered_set<U16> opponent_moves = b.get_legal_moves();
+    flip_player(b);
 
-    // for (auto move : opponent_moves) {
-    //     U8 final_pos = getp1(move);
-    //     for (int i = 0; i < 6; i++) {
-    //         if (final_pos == player_pieces[i]) {
-    //             score -= weights[i] / DEFENDING_FACTOR;
-    //         }
-    //     }
-    // }
-
-    if (mp[all_boards_to_str(b)] == 2) {
-        score = 0;
+    for (auto move : opponent_moves) {
+        U8 final_pos = getp1(move);
+        for (int i = 0; i < 6; i++) {
+            if (final_pos == player_pieces[i]) {
+                score -= weights[i] / DEFENDING_FACTOR;
+            }
+        }
     }
 
 
@@ -115,11 +113,9 @@ int eval(Board& b) {
 }
 
 
-int minimax(Board& board, int depth, bool maximizing_player, vector<Board*> &visited, int &alpha, int &beta) {
-    if (maximizing_player && mp[all_boards_to_str(board)] == 2) {
-        return INT_MAX;
-    } else if (!maximizing_player && mp[all_boards_to_str(board)] == 2) {
-        return INT_MIN;
+int minimax(Board& board, int depth, bool maximizing_player, vector<Board*> &visited, int alpha, int beta) {
+    if (mp[all_boards_to_str(board)] == 2) {
+        return (maximizing_player ? INT_MAX : INT_MIN);
     }
     if (depth == 0) {
         return eval(board);
@@ -157,21 +153,22 @@ int minimax(Board& board, int depth, bool maximizing_player, vector<Board*> &vis
             beta = min(beta, best_eval);
         }
         if (alpha >= beta) {
-            return best_eval;
+            break;
         }
     }
     return best_eval;
 }
 
 void Engine::find_best_move(const Board& b) {
+    auto start_time = chrono::high_resolution_clock::now();
     mp[all_boards_to_str(b)]++;
     if (curr_player == -1) {
         curr_player = b.data.player_to_play;
     }
     int best_eval = INT_MIN;
+    int best_move = 0;
     auto player_moveset = b.get_legal_moves();
     this->best_move = 0;
-    // std::vector<U16> moves;
     // std::cout << all_boards_to_str(b) << std::endl;
     // for (auto m : moveset) {
     //     std::cout << move_to_str(m) << " ";
@@ -192,8 +189,11 @@ void Engine::find_best_move(const Board& b) {
         free(new_board);
         if (eval > best_eval) {
             best_eval = eval;
-            this->best_move = move;
+            best_move = move;
         }
     }
+    this->best_move = best_move;
+    auto end_time = chrono::high_resolution_clock::now();
+    cout << "time taken to find best move: " << std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count() << " seconds" << endl;
     cout << "nodes visited " << nodes_visited << endl;
 }
